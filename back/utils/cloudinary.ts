918884432d1ee99926dac0fbf0d72aea01a09d49
Camila <ca.madureira094@gmail.com>
2024-11-15 09:22:39 -0300

    fix: fix cloudinary and tsconfig

--- back/utils/cloudinary.ts
+++ back/utils/cloudinary.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME, // Alterado
-  api_key: process.env.CLOUD_API_KEY, // Alterado
-  api_secret: process.env.CLOUD_SECRET_KEY, // Alterado
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
 export default cloudinary;
