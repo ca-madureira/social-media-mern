310c1e4d5e9e379d68a9fc7c888379d7eb2d1f2f
Camila <ca.madureira094@gmail.com>
2024-11-18 12:17:09 -0300

    chore: configure backendUrl

--- front/social/src/redux/api/apiSlice.ts
+++ front/social/src/redux/api/apiSlice.ts
@@ -1,18 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const backendUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://social-media-mern-9ans.vercel.app'
+    : 'http://localhost:8000';
+
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_REACT_APP_BACKEND_BASEURL,
+    baseUrl: backendUrl,
     credentials: 'include',
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
-
-      // Se o token existir, adicione o cabeçalho Authorization
       if (token) {
         headers.set('Authorization', `Bearer ${token}`);
       }
-
       return headers;
     },
   }),
