e02c13e03cab9bc5fe5d529f5384f09beebacdd5
Camila Madureira <ca.madureira094@gmail.com>
2025-02-23 19:07:53 -0300

    style: update layout of Forgot Password component

--- back/app.ts
+++ back/app.ts
@@ -1,33 +1,33 @@
-require('dotenv').config();
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import userRouter from './routes/user.route';
-import postRoute from './routes/postRoute';
-import authRoute from './routes/auth.route';
-import authRouter from './routes/auth.route';
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route";
+import postRoute from "./routes/postRoute";
+import authRoute from "./routes/auth.route";
+import authRouter from "./routes/auth.route";
 
 export const app = express();
 
 // app.use(express.json({ limit: '50mb' }));
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['https://social-media-mern-mu.vercel.app'],
-    methods: ['POST', 'GET'],
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+    methods: ["POST", "GET", "PUT"],
     credentials: true,
-  }),
+  })
 );
 
-console.log('Cloudinary Config:', {
+console.log("Cloudinary Config:", {
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
-app.use('/user', userRouter);
-app.use('/posts', postRoute);
-app.use('/auth', authRouter);
+app.use("/user", userRouter);
+app.use("/posts", postRoute);
+app.use("/auth", authRouter);
--- front/social/src/pages/ForgotPass.tsx
+++ front/social/src/pages/ForgotPass.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useState, useRef } from "react";
+import { useForm } from "react-hook-form";
 import {
   useSendForgotPasswordCodeMutation,
   useUpdatePassMutation,
   useVerifyCodeMutation,
-} from '../redux/auth/authApi';
-import { useNavigate } from 'react-router-dom';
+} from "../redux/auth/authApi";
+import { useNavigate } from "react-router-dom";
 
 const ForgotPass = () => {
+  const [otp, setOtp] = useState(["", "", "", "", ""]);
+
   const [step, setStep] = useState(1);
   const navigate = useNavigate();
   const [verifyCode] = useVerifyCodeMutation();
@@ -22,65 +24,92 @@ const ForgotPass = () => {
     getValues,
   } = useForm({
     defaultValues: {
-      email: '',
-      code: '',
-      password: '',
-      confirmPassword: '',
+      email: "",
+      code: "",
+      password: "",
+      confirmPassword: "",
     },
-    mode: 'onChange',
+    mode: "onChange",
   });
 
-  const handleSendEmail = async (data: any) => {
+  const otpRefs = useRef([]);
+
+  const handleSendEmail = async (data) => {
     await sendForgotPasswordCode({ email: data.email }).unwrap();
     setStep(2);
   };
 
-  const handleSendCode = async (data: any) => {
-    const email = getValues('email');
-    await verifyCode({ email, code: data.code }).unwrap();
+  const handleSendCode = async (otpCode) => {
+    const email = getValues("email");
+    await verifyCode({ email, code: otpCode }).unwrap();
     setStep(3);
   };
 
-  const handleSavePass = async (data: any) => {
-    const email = getValues('email');
+  const handleSavePass = async (data) => {
+    const email = getValues("email");
     await updatePass({ email, pass: data.password }).unwrap();
-    navigate('/login');
+    navigate("/login");
   };
 
-  const handleSend = (data: any) => {
+  const handleSend = (data) => {
     if (step === 1) {
       handleSendEmail(data);
     } else if (step === 2) {
-      handleSendCode(data);
+      // Aqui estamos enviando o código OTP como uma string
+      handleSendCode(otp.join("")); // Combina os valores do OTP e envia como string
     } else if (step === 3) {
       handleSavePass(data);
     }
   };
 
+  const handleOtpChange = (e, index) => {
+    const value = e.target.value;
+    if (/[^0-9]/.test(value)) return; // Prevent invalid input (only numbers)
+
+    const newOtp = [...otp];
+    newOtp[index] = value;
+    setOtp(newOtp);
+
+    if (value && index < otp.length - 1) {
+      otpRefs.current[index + 1].focus(); // Move to next input field
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && !otp[index]) {
+      if (index > 0) {
+        otpRefs.current[index - 1].focus(); // Move to previous input field
+      }
+    }
+  };
+
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-purple-500 font-mooli font-bold text-2xl ml-6">
-        {step === 1 && 'Enviar email'}
-        {step === 2 && 'Enviar código'}
-        {step === 3 && 'Alterar senha'}
-      </h1>
-      <section>
-        <form className="flex" onSubmit={handleSubmit(handleSend)}>
+    <main className="flex justify-center items-center h-screen">
+      <section className="bg-white shadow-lg flex flex-col lg:justify-center items-center w-[85%] lg:w-1/3 lg:h-[80%] p-4">
+        <h1 className="text-purple-500 font-mooli font-bold text-2xl ml-6">
+          {step === 1 && "Enviar email"}
+          {step === 2 && "Enviar código"}
+          {step === 3 && "Alterar senha"}
+        </h1>
+        <form
+          className="flex w-[100%] lg:w-[70%] mt-4 p-2"
+          onSubmit={handleSubmit(handleSend)}
+        >
           {step === 1 && (
-            <section className="flex flex-col gap-2">
+            <section className="flex flex-col gap-2 w-[100%]">
               <input
-                className="p-2 text-center font-mooli outline-none"
+                className="p-2 font-mooli outline-none border border-purple-500 rounded-md"
                 type="email"
                 id="email"
-                {...register('email', {
+                {...register("email", {
                   pattern: {
                     value:
                       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: 'Insira um email válido',
+                    message: "Insira um email válido",
                   },
                   required: {
                     value: true,
-                    message: 'Email é obrigatório',
+                    message: "Email é obrigatório",
                   },
                 })}
                 placeholder="Digite seu e-mail"
@@ -91,29 +120,32 @@ const ForgotPass = () => {
                 </p>
               )}
               <button
-                className="bg-purple-500 text-white font-semibold p-2"
+                className="bg-purple-500 text-white font-semibold p-2 rounded-md"
                 type="submit"
               >
-                {isLoading ? 'Enviando...' : 'Enviar e-mail'}
+                {isLoading ? "Enviando..." : "Enviar e-mail"}
               </button>
             </section>
           )}
 
           {step === 2 && (
-            <section className="flex flex-col gap-2">
-              <input
-                className="p-2 text-center font-mooli outline-none"
-                type="text"
-                {...register('code', {
-                  required: {
-                    value: true,
-                    message: 'Código é obrigatório',
-                  },
-                })}
-                placeholder="Digite o código"
-              />
+            <section className="flex flex-col gap-4 w-[100%]">
+              <div className="flex justify-between">
+                {otp.map((item, index) => (
+                  <input
+                    ref={(el) => (otpRefs.current[index] = el)} // Assigning ref
+                    type="text"
+                    maxLength={1}
+                    key={index}
+                    className="w-10 h-10  lg:w-10 lg:h-10 text-center rounded-md border-2 border-purple-400 outline-none text-purple-600 font-bold font-2xl"
+                    value={item}
+                    onChange={(e) => handleOtpChange(e, index)}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                  />
+                ))}
+              </div>
               <button
-                className="bg-purple-500 text-white font-semibold p-2"
+                className="bg-purple-500 text-white font-semibold rounded-md p-2"
                 type="submit"
               >
                 Enviar código
@@ -122,18 +154,18 @@ const ForgotPass = () => {
           )}
 
           {step === 3 && (
-            <section className="flex flex-col p-2 gap-2">
+            <section className="flex flex-col items-center p-2 gap-2 w-[100%]">
               <input
-                className="p-2 text-center font-mooli outline-none"
+                className="p-2 text-center font-mooli outline-none border border-purple-500 w-[80%] rounded-md"
                 type="password"
-                {...register('password', {
+                {...register("password", {
                   required: {
                     value: true,
-                    message: 'Senha é obrigatória',
+                    message: "Senha é obrigatória",
                   },
                   minLength: {
                     value: 6,
-                    message: 'Senha deve conter ao menos 6 caracteres',
+                    message: "Senha deve conter ao menos 6 caracteres",
                   },
                 })}
                 placeholder="Nova senha"
@@ -145,16 +177,16 @@ const ForgotPass = () => {
               )}
 
               <input
-                className="p-2 text-center font-mooli outline-none"
+                className="p-2 text-center font-mooli outline-none border border-purple-500 w-[80%] rounded-md"
                 type="password"
-                {...register('confirmPassword', {
+                {...register("confirmPassword", {
                   required: {
                     value: true,
-                    message: 'Confirmação de senha é obrigatória',
+                    message: "Confirmação de senha é obrigatória",
                   },
                   validate: (value) => {
-                    const password = getValues('password');
-                    return value === password || 'Senhas não coincidem';
+                    const password = getValues("password");
+                    return value === password || "Senhas não coincidem";
                   },
                 })}
                 placeholder="Confirme a nova senha"
@@ -165,7 +197,7 @@ const ForgotPass = () => {
                 </p>
               )}
               <button
-                className="bg-purple-500 text-white font-semibold p-2 text-center"
+                className="bg-purple-500 text-white font-semibold p-2 text-center w-[80%] rounded-md"
                 type="submit"
               >
                 Salvar senha
